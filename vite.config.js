6701d4391abfac9c88f4769568f07069a15ddad7
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-12-15 20:41:34 -0300

    Create Initial Architeture

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    fs: {
+      allow: [
+        searchForWorkspaceRoot(process.cwd()),
+        '/src',
+      ],
+
+    },
+  },
+
+});
