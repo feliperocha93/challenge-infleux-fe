ab69d90f95dba7d43cb7db77d72d5a9e60dde949
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-12-16 08:53:25 -0300

    Change styles lib

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,18 @@
+import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from '../../styles/global';
+import theme from '../../styles/theme';
 
 import Routes from '../../routes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Routes />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
